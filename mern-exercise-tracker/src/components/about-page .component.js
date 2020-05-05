7e339de25d41e4965271d498ada712207bed9694
Bggm4 <you@example.com>
2020-05-05 09:37:02 +0100

    edit

---
+++ mern-exercise-tracker/src/components/about-page .component.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+import "react-datepicker/dist/react-datepicker.css";
+
+export default class AboutClass extends Component {
+
+    render() {
+        return (
+        <div>
+          <h3>Create New Exercise Log</h3>
+          <p>This basic web system is to show creating, updating, deleting user items from a database. The stack used
+            was the MERN stack, this stack consists off using MongoDB, Express.js, React and finally Node.js. 
+            A backend server has been created to host the databsae. This is desgined for a user to be albe to track what
+            sort of exercises they are doing. User name, description of activity, duration of activity and the date will be saved.  
+
+          </p>
+
+          <h3>third party software used to carry this out</h3>
+          <p>The code was created in Visual Studio Code-https://code.visualstudio.com/</p>
+          <p>Database created on MongoDB</p>
+          <p>The code was created in Visual Studio Code</p>
+          <p>The code was created in Visual Studio Code</p>
+          </div>
+        )}
+}
